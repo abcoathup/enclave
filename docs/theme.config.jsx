12b16fd1427e1658f308e4ba31a03cbc5e307790
abcoathup <andrew@coathup.net>
2025-09-04 18:16:40 +1000

    docs: fix edit this page

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -18,7 +18,7 @@ export default {
   project: {
     link: "https://github.com/gnosisguild/enclave",
   },
-  docsRepositoryBase: "https://github.com/gnosisguild/enclave",
+  docsRepositoryBase: "https://github.com/gnosisguild/enclave/tree/main/docs",
   darkMode: false,
   nextThemes: {
     defaultTheme: "light",
